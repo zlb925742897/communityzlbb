e120366575f90e88ace6dd76723cf5b80a8df790
zhang <591411258@qq.com>
2019-11-15 09:26:18 +0800

    this is promit

---
+++ src/test/java/com/majiangcommunity/majiang/MajiangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majiangcommunity.majiang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajiangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
